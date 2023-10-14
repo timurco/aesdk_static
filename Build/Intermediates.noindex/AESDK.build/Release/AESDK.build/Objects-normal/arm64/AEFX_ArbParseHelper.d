c88f572c0931f9e0adf4f2c6933e601bc70564ca
Konstantin Shilyaev <timurko@gmail.com>
2023-10-14 05:01:59 +0300

    Removed unnecessary items

--- Build/Intermediates.noindex/AESDK.build/Release/AESDK.build/Objects-normal/arm64/AEFX_ArbParseHelper.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/timurko/Code/xcode/adobe-ae-sdk/Examples/Util/AEFX_ArbParseHelper.c \
-  ../../adobe-ae-sdk/Examples/Util/AEFX_ArbParseHelper.h \
-  ../../adobe-ae-sdk/Examples/Headers/AE_Effect.h \
-  ../../adobe-ae-sdk/Examples/Headers/A.h \
-  ../../adobe-ae-sdk/Examples/Headers/adobesdk/config/PreConfig.h \
-  ../../adobe-ae-sdk/Examples/Headers/adobesdk/config/PostConfig.h
